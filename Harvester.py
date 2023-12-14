54ad4dad841379b96fa74139f55a8753a3b1e399
Sm1mg <noahnmj@gmail.com>
2023-12-13 22:57:56 -0500

    implemented memory limit

--- Harvester.py
+++ Harvester.py
@@ -2,7 +2,10 @@ import aiohttp
 import asyncio
 import discord
 import hashlib
+import os
 import validators
+
+import Utils
 
 # Harvests hashed data sources from a message
 async def harvest_message(message: discord.Message) -> set[str]:
@@ -20,11 +23,25 @@ async def harvest_message(message: discord.Message) -> set[str]:
     if len(hashes) < len(message.attachments) + len(urls):
         # If a file couldn't be hashed by url, just hash the url
         for url in urls:
+            if Harvester.max_size != 0 and len(url) > Harvester.max_size:
+                Utils.pront("...Url length exceeded maximum memory usage limit, seriously??", "WARNING")
+                continue
             hashes.add(await Harvester.md5_hash_handler(url.encode()))
     return hashes
     
 
 class Harvester:
+    
+    # Parse max size from dotenv
+    if max_size := os.environ.get('max_file_size'):
+        try:
+            max_size = int(max_size)
+        except ValueError as e:
+            raise ValueError("Maximum filesize in .env must be integer-convertible")
+    else:
+        max_size = 0
+    
+
     def parse_urls(message: str) -> set[str]:
         urls = set()
         for word in message.split():
@@ -33,16 +50,24 @@ class Harvester:
         return urls
 
 
-    async def get_attachment_hash(attachment: discord.Attachment, hashes: set[str]) -> str:
-        hashes.add(await Harvester.md5_hash_handler(await attachment.read()))
+    async def get_attachment_hash(attachment: discord.Attachment, hashes: set[str]) -> None:
+        content = await attachment.read()
+        if Harvester.max_size != 0 and len(content) > Harvester.max_size:
+            Utils.pront("Attachment size exceeded maximum memory usage limit, aborting.", "WARNING")
+            return
+        hashes.add(await Harvester.md5_hash_handler(content))
 
     
-    async def file_hash_from_url(url: str, session: aiohttp.ClientSession, hashes: set[str]) -> str:
+    async def file_hash_from_url(url: str, session: aiohttp.ClientSession, hashes: set[str]) -> None:
         async with session.get(url) as response:
             if not response.ok:
                 return
             # Check for disallowed header content types
             if response.content_type.split("/")[0] in ["application", "font", "example", "message", "model", "multipart", "text"]:
+                return
+            content = await response.read()
+            if Harvester.max_size != 0 and len(content) > Harvester.max_size:
+                Utils.pront("URL response size exceeded maximum memory usage limit, aborting.", "WARNING")
                 return
             hashes.add(await Harvester.md5_hash_handler(await response.read()))
 
