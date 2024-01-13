TheSmimg/Smash-Goblins-2-for-the-PsVita-Plus

commit 77ceac0164c0b81eb1bed0ef07531a0de5080e67

fixed possible softlock

cogs/EventsCog.py
@@ -25,8 +25,6 @@ class EventsCog(commands.Cog):
         if not reaction.me:
             return
 
-        await message.delete()
-
         reference = message.reference.resolved
 
         if reference is None or isinstance(reference, discord.DeletedReferencedMessage):
@@ -34,8 +32,13 @@ class EventsCog(commands.Cog):
             return
 
         watcher = Servers.get_watcher(reference.guild.id)
-        await watcher.blacklist(reference)
-
+        if not watcher:
+            return
+        if watcher.channel != message.channel:
+            return
+        
+        await message.delete()
+        await watcher.blacklist(message.guild.id)
         await reference.reply(embed=discord.Embed(title='Blacklisted.'), delete_after=5, mention_author=False)
 
     @commands.Cog.listener()
@@ -47,7 +50,6 @@ class EventsCog(commands.Cog):
             return
         if watcher.channel != message.channel:
             return
-        
         # Task worker that handles calling out users for reposts
         async def repost(match: list[str]):
             try:

Watcher.py
@@ -17,11 +17,11 @@ class Watcher:
         
         Methods
         -------
-        async process(message: `discord.Message`) -> Generator[list[str], None, None]:
+        async process(message: `discord.Message`) -> `Generator`[`list`[`str`], `None`, `None`]:
             A generator that processes the supplied message and checks if its media is a repost.
         async blacklist(message: `discord.Message`):
             Adds the supplied message to the internal blacklist.
-        async raw_delete(payload: discord.RawMessageDeleteEvent):
+        async raw_delete(payload: `discord.RawMessageDeleteEvent`):
             Processes a RawMessageDeleteEvent to avoid detecting deleted messages as reposts.
         
             
@@ -39,10 +39,7 @@ class Watcher:
             async with self._lock:
                 async for msg in channel.history(limit=None, oldest_first=True):
                     for source in await Harvester.harvest_message(msg):
-                        print(msg.content)
                         # If this hash is already here, append instead of replacing
-                        if self._hashes.get(source) is not None:
-                            self._hashes[source].append(msg.jump_url)
                             continue
                         self._hashes[source] = [msg.jump_url]
             # Set bot to appear online
@@ -69,10 +66,10 @@ class Watcher:
         `list`[`str`]:
             A list of all matches of each source, if any exist.
         """
-        async with self._lock:
-            for source in await Harvester.harvest_message(message):
-                print(f"process {message.content}")
-                if self._blacklist.get(source) is not None:
+        # This can cause some weird results, but it'll avoid a softlock
+        for source in await Harvester.harvest_message(message):
+            async with self._lock:
+                if self._blacklist.get(source):
                     continue
                 # No match
                 if (match := self._hashes.get(source)) is None:
@@ -80,8 +77,7 @@ class Watcher:
                     continue
                 # Match & permitted
                 self._hashes[source].append(message.jump_url)
-                yield match
-        return
+            yield match
     
 
     async def blacklist(self, message: discord.Message) -> None:
@@ -100,7 +96,10 @@ class Watcher:
                 if self._blacklist.get(source):
                     continue
                 self._blacklist[source] = message.jump_url
-                self._hashes.pop(source)
+                try:
+                    self._hashes.pop(source)
+                except:
+                    pass
 
     
     async def raw_delete(self, payload: discord.RawMessageDeleteEvent) -> None:

commit 60e76e8394e633501509f886ff3d211e9156da2f

fixed bug introduced in commit 77ceac0

cogs/EventsCog.py
@@ -38,7 +38,7 @@ class EventsCog(commands.Cog):
             return
         
         await message.delete()
-        await watcher.blacklist(message.guild.id)
+        await watcher.blacklist(message)
         await reference.reply(embed=discord.Embed(title='Blacklisted.'), delete_after=5, mention_author=False)
 
     @commands.Cog.listener()